2b581809687ad7bc16dfcc0cc27518a24dcd7823
Adle Ben Salem <adlesalem@hotmail.fr>
2024-02-10 16:48:32 +0100

    affichage + var break + var match interpots

--- template.jl
+++ template.jl
@@ -20,6 +20,10 @@ T = 8
 @variable(model, two_matches_potC[1:T], Bin)
 @variable(model, two_matches_potD[1:T], Bin)
 
+# Variables supplémentaires pour les matchs entre les pots A et B et d'autre part C et d
+@variable(model, three_matches_potsAB[1:T], Bin)
+@variable(model, three_matches_potsCD[1:T], Bin)
+
 
 # Contrainte : une équipe ne peut pas jouer contre elle-même
 @constraint(model, no_self_play[i in 1:N, t in 1:T], x[i, i, t] == 0)
@@ -37,6 +41,20 @@ for i in 1:N
         @constraint(model, sum(x[j, i, t] for t in 1:T, j in pot_start:pot_start+8) == 1)
     end
 end
+
+# Contrainte pour s'assurer de l'homogénéité de la difficulté au cours du temps
+# On s'assure d'avoir exactement 2 matchs contre A et B les quatres premiers jours
+for i in 1:N
+    @constraint(model, sum(x[i, j, t] + x[j, i, t] for j in 1:18 for t in 1:4) == 2)
+end
+
+#On joue une fois contre 1 equipe du pot A dans la première moitié = IMPOSSIBLE
+#for i in 1:N
+#    @constraint(model, sum(x[i, j, t] + x[j, i, t] for j in 1:9 for t in 1:4) == 1)
+#    @constraint(model, sum(x[i, j, t] + x[j, i, t] for j in 28:36 for t in 1:4) == 1)
+#end
+
+
 
 # Contrainte pour l'alternance stricte au début et à la fin
 @constraint(model, strict_alternate_start[i in 1:N], sum(x[i, j, 1] + x[i, j, 2] for j in 1:N) == 1)
@@ -66,16 +84,30 @@ for t in 1:T
 end
 
 
-# Fonction pour ajouter une contrainte de matchs maximum entre deux pots par journée
+
+# Contraintes pour s'assurer qu'il y a exactement 2 jour avec 3 matchs inter-pots pour les couples (A,B) et (C,D)
+@constraint(model, sum(three_matches_potsAB[t] for t in 1:T) == 2)
+@constraint(model, sum(three_matches_potsCD[t] for t in 1:T) == 2)
+
+
+# Contraintes pour s'assurer que chaque jour a ou bien 2 ou bien 3 matchs inter-pots (A,B) et (c,D)
+for t in 1:T
+    @constraint(model, sum(x[i, j, t] + x[j, i, t] for i in 1:9, j in 10:18) == 2 + three_matches_potsAB[t])
+    @constraint(model, sum(x[i, j, t] + x[j, i ,t] for i in 19:27, j in 28:36) == 2 + three_matches_potsCD[t])
+end
+
+# Fonction pour ajouter une contrainte de matchs minimum maximum entre deux pots par journée
 function add_max_inter_pot_constraint(pot1_start, pot1_end, pot2_start, pot2_end, min_matches, max_matches)
     for t in 1:T
-        @constraint(model, min_matches <= sum(x[i, j, t] for i in pot1_start:pot1_end, j in pot2_start:pot2_end) + 
-                            sum(x[i, j, t] for i in pot2_start:pot2_end, j in pot1_start:pot1_end) <= max_matches)
+        @constraint(model, min_matches <= sum(x[i, j, t] + x[j, i, t] for i in pot1_start:pot1_end, j in pot2_start:pot2_end)
+                 <= max_matches)
     end
 end
 
-# Appliquer la contrainte pour chaque paire de pots avec un maximum de 3 matchs par journée
-add_max_inter_pot_constraint(1, 9, 10, 18, 2, 3) # Pots A et B
+
+# Appliquer la contrainte pour chaque paire de pots avec un maximum de 3 matchs par journée, (non obligatoire
+#                                                                                              pour les pots A et B et C et D)
+add_max_inter_pot_constraint(1, 9, 10, 18, 2, 3) # Pots A et B 
 add_max_inter_pot_constraint(1, 9, 19, 27, 2, 3) # Pots A et C
 add_max_inter_pot_constraint(1, 9, 28, 36, 1, 3) # Pots A et D
 add_max_inter_pot_constraint(10, 18, 19, 27, 1, 3) # Pots B et C
@@ -104,7 +136,11 @@ encourage_sequential_matches(model, 10, 18) # Pot B
 encourage_sequential_matches(model, 19, 27) # Pot C
 encourage_sequential_matches(model, 28, 36) # Pot D
 
-@constraint(model, sum(break_var[i, t] for i in 1:N, t in 2:6) == 5)
+#Contrainte sur le nombre de break, on cherche une solution a 4 break, une chaque pour groupe
+@constraint(model, sum(break_var[i, t] for i in 1:9, t in 2:6) == 1)
+@constraint(model, sum(break_var[i, t] for i in 10:18, t in 2:6) == 1)
+@constraint(model, sum(break_var[i, t] for i in 19:27, t in 2:6) == 1)
+@constraint(model, sum(break_var[i, t] for i in 28:36, t in 2:6) == 1)
 
 #@objective(model, Min, sum(break_var[i, t] for i in 1:N, t in 2:6))
 
